eea3e7f0a733780ce2f3d544d35c2dd8966d0d26
xiaozhou23 <287133294@qq.com>
2018-05-31 14:22:47 +0800

    Initial commit

---
+++ demo01/test01.py
@@ -0,0 +1,32 @@
+# 定义一个空类
+class Student():
+    pass
+
+
+# 定义一个对象
+Lily = Student()
+
+
+# 定义一个类，学习Python的学生
+class StudentPython():
+    name = None
+    age = 18
+    course = "Python"
+
+    def doHomework(self):
+        print("I am doing homework")
+        # 末尾加return
+        return None
+
+
+# 实例化对象
+sam = StudentPython()
+print(sam.name)
+print(sam.age)
+
+# 调用类函数，没有参数传入
+sam.doHomework()
+
+print(StudentPython.__dict__)
+
+print(sam.__dict__)
